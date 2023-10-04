e891d2f91e55ed8eb214c940b37772b04ac43965
evafairelavaiselle <146078439+evafairelavaiselle@users.noreply.github.com>
2023-10-04 18:05:49 +0200

    04

--- ex_04.js
+++ ex_04.js
@@ -1,14 +1,9 @@
-function capitalize(arr) {
-    return arr.map(word => word.toUpperCase());
+function capitalize(maj) {
+    return maj.map(maj => maj.toUpperCase())
   }
   
-  const inputArray = ['CapITAlize', 'tHis', 'text'];
-  const resultArray = capitalize(inputArray);
-  console.log(resultArray);
+  console.log(capitalize(['CapITAlize', 'tHis', 'text']))
   
-  
-  
-  // Here is some code to test and display your function output:
-  // (you must modify it to test out your code but /!\ do not deliver it /!\ ). Only deliver your function
   const result = ["CHANGE", "this", "array", "BY", "ThE", "RESULT", "oF", "youR", "FUNCTION", "TO", "test", "IT", "OUT"]
   displayResult(capitalize(result))
+  
